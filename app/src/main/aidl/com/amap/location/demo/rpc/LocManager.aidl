0a0317e868e062856b3a524b51ff06d21ad23752
wangfengye <1040441325@qq.com>
2019-05-11 17:41:16 +0800

    rpc通信

---
+++ app/src/main/aidl/com/amap/location/demo/rpc/LocManager.aidl
@@ -0,0 +1,12 @@
+// LocManager.aidl
+package com.amap.location.demo.rpc;
+
+// Declare any non-default types here with import statements
+
+interface LocManager {
+    /**
+     * Demonstrates some basic types that you can use as parameters
+     * and return values in AIDL.
+     */
+    String loc();
+}
--- app/src/main/java/com/maple/maplexpose/MainActivity.java
+++ app/src/main/java/com/maple/maplexpose/MainActivity.java
@@ -1,7 +1,15 @@
 package com.maple.maplexpose;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,12 +22,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amap.location.demo.rpc.LocManager;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
     public static TextView mTv;
     APList mAps;
+    private Messenger mService;
+    private Messenger mMessenger = new Messenger(new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            Log.i(TAG, "handleMessage: "+ msg.getData().get("loc"));
+            super.handleMessage(msg);
+        }
+    });
+    private LocManager mLocManger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,16 +81,80 @@ public class MainActivity extends AppCompatActivity {
                 showText();
             }
         });
+        findViewById(R.id.btn_loc).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // sendBroad();
+               // sendWithMEssenger();
+                sendWithAidl();
+
+            }
+        });
+        bindService();
     }
-
+    private void sendBroad(){
+        Intent intent = new Intent();
+        intent.setAction("com.maple.maplexpose.loc");
+        sendBroadcast(intent);
+    }
     private void showText() {
-
         mTv.setText(mAps.toString());
     }
+    private void bindService(){
+        ServiceConnection connection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                mService = new Messenger(service);
+            }
 
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                mService =null;
+            }
+        };
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.HOOKMESSENGER");
+        intent.setPackage("com.amap.location.demo");
+        bindService(intent,connection,Context.BIND_AUTO_CREATE);
+        bindAidl();
+    }
+    private void sendWithMEssenger(){
+        Message msgFromClient = new Message();
+        msgFromClient.what = 1;
+        msgFromClient.replyTo = mMessenger;
+        try {
+            mService.send(msgFromClient);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     protected void onDestroy() {
         mTv = null;
         super.onDestroy();
+    }
+    private void bindAidl(){
+        ServiceConnection connection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                mLocManger = LocManager.Stub.asInterface(service);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                mLocManger =null;
+            }
+        };
+        Intent intent = new Intent();
+        intent.setAction("com.maple.aidl");
+        intent.setPackage("com.amap.location.demo");
+        bindService(intent,connection,Context.BIND_AUTO_CREATE);
+    }
+    private void sendWithAidl(){
+        try {
+            Log.i(TAG, "sendWithAidl: "+mLocManger.loc());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
